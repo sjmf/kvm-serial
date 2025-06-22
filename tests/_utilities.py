8f0e7a6b634b3d8955407f91fb742e1cc52ee748
Samantha Finnigan <1038320+sjmf@users.noreply.github.com>
2025-06-22 13:45:32 +0100

    Add a fixture which patches `isinstance()`, useful for our Keyboard tests.

--- tests/_utilities.py
+++ tests/_utilities.py
@@ -22,3 +22,28 @@ class MockSerial:
 @fixture
 def mock_serial():
     return MockSerial()
+
+
+@fixture
+def patch_isinstance_for_serial(mock_serial):
+    """
+    Keyboard __init__ makes extensive use of isinstance() for typechecking
+    This is a /huge/ pain to test.
+    So, patch isinstance() to treat mock_serial as Serial.
+    This is nicer to test, albeit terrifying to look at.
+    """
+    import builtins
+
+    orig_isinstance = builtins.isinstance
+
+    def fake_isinstance(obj, typ):
+        if obj is mock_serial and getattr(typ, "__name__", None) == "Serial":
+            return True
+        return orig_isinstance(obj, typ)
+
+    builtins.isinstance = fake_isinstance
+
+    try:
+        yield
+    finally:
+        builtins.isinstance = orig_isinstance
